27322b017c5608f84e32c5592eec03275900e62f
weihung_lai <u9908028@gmail.com>
2022-05-06 00:17:06 +0800

    upload home and bootstrap

--- src/components/contents/home/Home.js
+++ src/components/contents/home/Home.js
@@ -1,14 +1,60 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row, Card } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';//この行
+import styled, { keyframes } from "styled-components";
+
+const colorChangeText = keyframes`
+    0% { color: red; }
+    33% { color: green; }
+    66% { color: blue;}
+    100% { color: red; }
+`;
+const TitleDiv = styled.div`
+    font-size: 20px;
+    text-align: center;
+    padding: 10px 0;
+    border-bottom: 0.5px dotted darkgreen;
+    margin: 0 0 10px 0;
+    animation: ${colorChangeText} 2s linear infinite;
+`;
+
+const MemoDiv = styled.div`
+
+`;
 
 function Home() {
-    return(
+    return (
         <React.StrictMode>
-            <div>Home Page</div>
-            <Button>TTYY</Button>
+            <TitleDiv>
+                ホームはあえて雑なプログラミングメモを置くスタイル！
+            </TitleDiv>
+            <MemoDiv>
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card style={{ width: '100%' }}>
+                                <Card.Img variant="top" src="" />
+                                <Card.Body>
+                                    <Card.Title>React</Card.Title>
+                                    <Card.Text>
+                                    初心者のReactメモです。<br/>
+                                    あまり期待しないでください。<br/>
+                                    現場との大きく違いがあるかもしれません。
+                                    </Card.Text>
+                                    <Button variant="primary">Reactのメモを見る</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>Javascript</Col>
+                    </Row>
+                    <Row>
+                        <Col>HTML</Col>
+                        <Col>CSS</Col>
+                        <Col>UED</Col>
+                    </Row>
+                </Container>    
+            </MemoDiv>
         </React.StrictMode>
-        
     );
 };
 export default Home;
