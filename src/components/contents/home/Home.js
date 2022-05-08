04dd9c7a3d1227c5df86ad8152e2ba6bf09c0fbf
weihung_lai <u9908028@gmail.com>
2022-05-08 09:38:39 +0800

    home test upload

--- src/components/contents/home/Home.js
+++ src/components/contents/home/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Button, Col, Container, Row, Card } from "react-bootstrap";
+import { Button, Col, Container, Row, Card, Carousel } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';//この行
 import styled, { keyframes } from "styled-components";
+import profileIcon from "../../../images/ojigi_animal_inu.png";
+import { QuickAccessData } from "./QuickAccessData";
 
 const colorChangeText = keyframes`
     0% { color: red; }
@@ -17,43 +19,66 @@ const TitleDiv = styled.div`
     margin: 0 0 10px 0;
     animation: ${colorChangeText} 2s linear infinite;
 `;
+const StyleImg = styled.img`
+    width: 300px;
+    height: auto;
+`;
 
 const MemoDiv = styled.div`
 
 `;
+const InfoDiv = styled.div`
+    text-align: center;
+    padding: 20px 10px 10px 10px;
+    border: 1px solid black;
+`;
+const QuickAccessDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: scretch;
+    box-sizing: border-box;
+`;
+const StyledCard = styled(Card)`
+    background: grey;
+    width: 30%;
+    display: inline-block;
+`;
+
 
 function Home() {
     return (
         <React.StrictMode>
-            <TitleDiv>
-                ホームはあえて雑なプログラミングメモを置くスタイル！
-            </TitleDiv>
-            <MemoDiv>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Card style={{ width: '100%' }}>
-                                <Card.Img variant="top" src="" />
-                                <Card.Body>
-                                    <Card.Title>React</Card.Title>
-                                    <Card.Text>
-                                    初心者のReactメモです。<br/>
-                                    あまり期待しないでください。<br/>
-                                    現場との大きく違いがあるかもしれません。
-                                    </Card.Text>
-                                    <Button variant="primary">Reactのメモを見る</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>Javascript</Col>
-                    </Row>
-                    <Row>
-                        <Col>HTML</Col>
-                        <Col>CSS</Col>
-                        <Col>UED</Col>
-                    </Row>
-                </Container>    
-            </MemoDiv>
+            <Container>
+                <Row>
+                    <Col>
+                        <InfoDiv>
+                            <h2>最新情報</h2>
+                        </InfoDiv>
+                    </Col>
+                    <Col>
+                        <InfoDiv>
+                            <h2>クイックアクセス</h2>
+                            <QuickAccessDiv>
+                                {
+                                    QuickAccessData.map((value, key) => {
+                                        return(
+                                            <StyledCard key={key}>
+                                                <Card.Img variant="top" src={value.icon} />
+                                                <Card.Body>
+                                                    <Button variant="primary">{value.title}</Button>
+                                                </Card.Body>
+                                            </StyledCard>
+                                        );
+                                    })
+                                }
+                            </QuickAccessDiv>
+                        </InfoDiv>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>プログラミングメモ</Col>
+                </Row>
+            </Container>
         </React.StrictMode>
     );
 };
---
+++ src/components/contents/home/QuickAccessData.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const QuickAccessData = [
+    {
+        title: 'プライバシー',
+        icon: '',
+        url: ''
+    },
+    {
+        title: 'お問い合わせ',
+        icon: '',
+        url: ''
+    },
+    {
+        title: 'プロフィール',
+        icon: '',
+        url: ''
+    },
+    {
+        title: 'Reactメモ',
+        icon: '',
+        url: ''
+    },
+];
